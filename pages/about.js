63a8fba5e36aa5a20c08fb94baaabfe91c296729
cesar magallon <cesar9mv@gmail.com>
2019-09-15 18:36:34 -0500

    [add]mdx

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import Layout from '../components/Layout';
+import Content from '../components/Content';
+import Header from '../components/Header';
+import Card from '../components/Card';
+
+const About = () => (
+    <Layout>
+        <Content>
+            <Header>About</Header>
+            <Card>
+                this is a paragraph
+            </Card>
+        </Content>
+    </Layout>
+)
+
+export default About;
